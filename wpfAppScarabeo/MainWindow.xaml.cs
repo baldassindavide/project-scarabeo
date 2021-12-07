5713a8c90a4e2abda901f3f09fad8612b8cb015d
baldassindavide <baldassindavidescuola@gmail.com>
2021-12-07 09:59:28 +0100

    aggiunto track mouse
    potrebbe essere utile per capire quale casella viene premuta

--- wpfAppScarabeo/MainWindow.xaml.cs
+++ wpfAppScarabeo/MainWindow.xaml.cs
@@ -20,9 +20,36 @@ namespace wpfAppScarabeo
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int X;
+        private int Y;
         public MainWindow()
         {
             InitializeComponent();
+            Label label = new Label();
+            label.Width = 100;
+            label.Height = 30;
+
+            this.Content = label;
+
+            this.Loaded += delegate
+            {
+                System.Timers.Timer timer = new System.Timers.Timer();
+                timer.Elapsed += delegate
+                {
+                    this.Dispatcher.Invoke(new Action(delegate
+                    {
+                        Mouse.Capture(this);
+                        Point pointToWindow = Mouse.GetPosition(this);
+                        Point pointToScreen = PointToScreen(pointToWindow);
+                        label.Content = pointToScreen.ToString();
+                        Mouse.Capture(null);
+                    }));
+                };
+                timer.Interval = 1;
+                timer.Start();
+            };
         }
+
+        
     }
 }
